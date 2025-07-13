1ca996adee491665e7dd47b35659dd588eb4fb3a
Roger Westerbø <roger.westerbo@gmail.com>
2025-07-13 13:22:46 +0200

    changing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  ssr: {
+    noExternal: ["primereact", "primeicons"],
+  },
+  optimizeDeps: {
+    include: ["primereact/api", "primereact/**"],
+  },
 });
